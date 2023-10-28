d2150ee91ff10cb84c47ce13d46643e4e4047fb5
Moutaz <aboseadamoutaz@gmail.com>
2023-10-28 22:29:44 +0300

    new gam epass

--- awzan.py
+++ awzan.py
@@ -21,9 +21,10 @@ with min_col:
 with max_col:
     max = st.text_input("Minimum Weight = ", str(max))
 
+new_game_pass = st.text_input('New Game password','')
 reset = st.button("New Game", type="primary")
 
 
-if reset:
+if reset== True and new_game_pass == '701':
     game.reset(int(min), int(max))
 st.text(game.get_reset_info())
